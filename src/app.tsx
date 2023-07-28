2988993d98cb70b52521a6d40b95cac52c4baf47
nakasyou <syoutarou1103@outlook.jp>
2023-07-28 01:54:22 +0000

    Add tailwind

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import { useState } from 'preact/hooks'
 import preactLogo from './assets/preact.svg'
 import viteLogo from '/vite.svg'
 import './app.css'
+import './tailwind.css'
 
 export function App() {
   const [count, setCount] = useState(0)
